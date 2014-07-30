c200acd719fa23134a07cc1ad32c82363c1e0412
GitHub Teacher <trainingdemos+githubteacher@github.com>
2014-07-30 15:43:21 -0700

    test message wasn't clear

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -1,7 +1,7 @@
 require './Square.rb'
 
 describe Square do
-  it "should respond to length" do
+  it "should respond to side" do
     square = Square.new(6)
     expect(square).to respond_to(:side)
   end
